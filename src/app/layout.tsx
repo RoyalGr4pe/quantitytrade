568877ad89e6c4ffb9eacfa1e055bbb5e74f830e
RoyalGr4pe <njames.programming@gmail.com>
2024-11-21 11:38:17 +0000

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,43 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: "Quantity Trade",
-	description: "Trading Fast and Smart - Tools designed to maximize profits and minimize loses.",
+  title: "Quantity Trade",
+  description: "Trading Fast and Smart - Tools designed to maximize profits and minimize losses.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<div
-					className="min-h-screen"
-					style={{ backgroundImage: 'linear-gradient(90deg, #019267, #151D3B)', backgroundSize: 'cover', backgroundPosition: 'center' }}
-				>
-					<div className="flex items-center justify-center min-h-screen">
-						{children}
-					</div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{
+          backgroundImage: "linear-gradient(90deg, #019267, #151D3B)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
